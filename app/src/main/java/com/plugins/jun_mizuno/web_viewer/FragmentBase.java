4f68da899259d152dafd79976f559f9710285680
水野淳 <>
2020-01-26 16:55:12 +0900

    フラグメントクラスの追加

---
+++ app/src/main/java/com/plugins/jun_mizuno/web_viewer/FragmentBase.java
@@ -0,0 +1,21 @@
+package com.plugins.jun_mizuno.web_viewer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public abstract class FragmentBase extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+    }
+}
